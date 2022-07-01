6fd0acd1962a898894b60e569345e868d1264975
Tiago <tiago@gobacklog.com>
2022-06-30 23:17:18 -0300

    Image aspect ratio

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Ratio from 'react-bootstrap/Ratio';
 
 const Post = ({post}) => (
-   <Card>
-      <Card.Img variant="top" src={'https://instagramshop.s3.amazonaws.com/'+post.image} />
+   <Card className="mb-3">
+      <Ratio aspectRatio="1x1">
+        <embed type="image/jpg" src={'https://instagramshop.s3.amazonaws.com/'+post.image} />
+      </Ratio>
       <Card.Body>
         <Card.Title>{post.title}</Card.Title>
         <Card.Text>
