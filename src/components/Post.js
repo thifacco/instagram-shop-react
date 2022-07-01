c2c3c4bb655bb83f2e203b1e095405a3cebbfe8f
Tiago <tiago@gobacklog.com>
2022-06-30 23:24:35 -0300

    Row gutters

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,16 +3,16 @@ import Card from 'react-bootstrap/Card';
 import Ratio from 'react-bootstrap/Ratio';
 
 const Post = ({post}) => (
-   <Card className="mb-3">
+   <Card className="mb-md-2">
       <Ratio aspectRatio="1x1">
         <embed type="image/jpg" src={'https://instagramshop.s3.amazonaws.com/'+post.image} />
       </Ratio>
-      <Card.Body>
+      {/* <Card.Body>
         <Card.Title>{post.title}</Card.Title>
         <Card.Text>
           {post.description}
         </Card.Text>
-      </Card.Body>
+      </Card.Body> */}
     </Card>
 );
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Post from './Post'
 
 const Posts = ({products}) => (
-   <Row className="my-5">
+   <Row className="my-5 g-0 g-md-3">
       {products.map((product, index) => (
          <Col xs={6} md={4}>
             <Post post={product}/>
