c6ef208034ce240de1e1563bb171db5091c624a2
Tiago <tiago@gobacklog.com>
2022-07-07 07:30:26 -0300

    Load product page

--- src/components/Permalink.js
+++ src/components/Permalink.js
@@ -5,14 +5,16 @@ import {
   Route,
   useParams
 } from "react-router-dom";
+import axios from "axios";
 import Posts from './Posts';
 import Page2 from './Page2';
+import Post from "./Post";
 
 export default function Permalink(props) {
   return (
     <Router>
       <Routes>
-        <Route path="/:id" element={<Product />} />
+        <Route path="/:id" element={<Product products={props.products} />} />
         <Route path="/" element={<Posts products={props.products} />} />
         <Route path="/page2" element={<Page2 />} />
       </Routes>
@@ -20,12 +22,21 @@ export default function Permalink(props) {
   );
 }
 
-function Product() {
-  let {id} = useParams();
+function Product(props) {
+  const {id} = useParams();
+  const productSelected = props.products.filter(product => product.id === id);
+
+  let product;
+  if (typeof productSelected[0] !== 'undefined') {
+    product = productSelected[0];
+  }
 
   return (
     <div>
       <h1>Post ID: {id}</h1>
+      {product?.title}
+
+      {(product) ? <Post post={product}/> : ''}
     </div>
   );
 }
