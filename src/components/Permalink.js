70322c045cf9aa5e9e942a48a387052660ce50d8
Tiago <tiago@gobacklog.com>
2022-07-07 07:02:20 -0300

    Permalink

--- src/components/Permalink.js
+++ src/components/Permalink.js
@@ -3,57 +3,26 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
   useParams
 } from "react-router-dom";
 import Posts from './Posts';
 import Page2 from './Page2'
 import Product from './Product';
 
-// Params are placeholders in the URL that begin
-// with a colon, like the `:id` param defined in
-// the route in this example. A similar convention
-// is used for matching dynamic segments in other
-// popular web frameworks like Rails and Express.
-
 export default function Permalink(props) {
   return (
     <Router>
-      <div>
-        <h2>Accounts</h2>
-
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/netflix">Netflix</Link>
-          </li>
-          <li>
-            <Link to="/zillow-group">Zillow Group</Link>
-          </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/modus-create">Modus Create</Link>
-          </li>
-        </ul>
-
-        <Routes>
-          <Route path="/:id" element={<Child />} />
-          <Route path="/" element={<Posts products={props.products} />} />
-          <Route path="/:id" element={<Product />} />
-          <Route path="/page2" element={<Page2 />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/:id" element={<Child />} />
+        <Route path="/" element={<Posts products={props.products} />} />
+        <Route path="/:id" element={<Product />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </Router>
   );
 }
 
 function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
   let { id } = useParams();
 
   return (
